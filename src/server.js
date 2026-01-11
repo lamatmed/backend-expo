a2b1e43959d95e76fdb99313725626ef6660643d
Lamat Abdellahi <lamat032025@gmail.com>
2026-01-11 11:22:08 +0100

    inits

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { serve } from "inngest/express";
 import cors from "cors";
 
 import { functions, inngest } from "./config/inngest.js";
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
@@ -17,24 +18,26 @@ import cartRoutes from "./routes/cart.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 
 const app = express();
+
 const __dirname = path.resolve();
 
-/* Stripe webhook (RAW body) */
+// special handling: Stripe webhook needs raw body BEFORE any body parsing middleware
+// apply raw body parser conditionally only to webhook endpoint
 app.use(
   "/api/payment",
   (req, res, next) => {
     if (req.originalUrl === "/api/payment/webhook") {
       express.raw({ type: "application/json" })(req, res, next);
     } else {
-      express.json()(req, res, next);
+      express.json()(req, res, next); // parse json for non-webhook routes
     }
   },
   paymentRoutes
 );
 
 app.use(express.json());
-app.use(clerkMiddleware());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
@@ -48,21 +51,31 @@ app.use("/api/cart", cartRoutes);
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
+app.get("/", (req, res) => {
+  res.json({
+    status: "Backend is running 🚀",
+  });
+});
 
-/* Frontend build */
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("*", (req, res) => {
+    app.get("*", (req, res) => {
+    // Si la route commence par /api, renvoyer 404
     if (req.path.startsWith("/api/")) {
       return res.status(404).json({ error: "API endpoint not found" });
     }
-    res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+    // Sinon, servir le frontend
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
 
-/* DB connection (OK sur Vercel) */
-await connectDB();
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
 
-/* ❌ PAS DE app.listen() SUR VERCEL */
-export default app;
+startServer();
