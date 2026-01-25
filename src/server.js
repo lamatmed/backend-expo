c5d8851d7f6623a9a637007fd80bc9e095eff5e3
Lamat Abdellahi <lamat032025@gmail.com>
2026-01-25 21:18:55 +0100

    inits

--- src/server.js
+++ src/server.js
@@ -37,7 +37,10 @@ app.use(
 
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+app.use(cors({
+  origin: [ENV.CLIENT_URL, "https://e-commerce-admin-six-vert.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
